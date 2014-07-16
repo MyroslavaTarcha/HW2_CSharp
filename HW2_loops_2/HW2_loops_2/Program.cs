f3cc640f6a0682640d0d1d9db711ac915acf0a70
MyroslavaTarcha <tarcham1993@gmail.com>
2014-07-16 08:51:37 +0300

    Initial commit to repo

--- HW2_loops_2/HW2_loops_2/Program.cs
+++ HW2_loops_2/HW2_loops_2/Program.cs
@@ -10,6 +10,72 @@ namespace HW2_loops_2
     {
         static void Main(string[] args)
         {
+           //будуємо рівносторонній трикутник. довжина сторони а=18, висота h=16.
+           //відповідно, будуючи рівняння прямих, що є сторонами такого трикутника, получаємо наступні нерівності для самого трикутника:
+           // y>(144-16x)/9; y>(16x-144)/9; y<16;
+            Console.WriteLine("The equilateral triangle\n");
+            for (int i = 0; i <= 18; i++)
+            {
+                for (int j = 0; j <= 16; j++)
+                {
+                    if (j >= (144 - 16 * i) / 9 && j >= (16 * i - 144) / 9)
+                    {
+                        Console.Write("*");
+                        continue;
+                    }
+                    Console.Write(" ");
+                }
+                Console.Write("\n");
+            }
+            //Будуємо прямокутний трикутник.
+            Console.WriteLine("The right triangle\n");
+            for (int i = 0; i <= 18; i++)
+            {
+                for (int j = 0; j <= 16; j++)
+                {
+                    if (j <= (144 - 16 * i) / 9 )
+                    {
+                        Console.Write("*");
+                        continue;
+                    }
+                    Console.Write(" ");
+                }
+                Console.Write("\n");
+            }
+
+            //Квадрат є частковим випадком ромба =)
+
+            Console.WriteLine("The rhombus\n");
+
+
+            for (int i = 0; i <= 18; i++)
+            {
+                for (int j = 0; j <= 18; j++)
+                {
+                    
+                        Console.Write("* ");
+                        
+                    
+                }
+                Console.Write("\n");
+            }
+
+            Console.WriteLine("The rectangle\n");
+
+
+            for (int i = 0; i <= 18; i++)
+            {
+                for (int j = 0; j <= 30; j++)
+                {
+
+                    Console.Write("* ");
+
+
+                }
+                Console.Write("\n");
+            }
+
+            Console.ReadKey();
         }
     }
 }
---
+++ HW2_loops_3/HW2_loops_3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW2_loops_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input a count of clients (integer > 0)");
+            Int32 N=Convert.ToInt32(Console.ReadLine());
+            if (N <= 0)
+            {
+                Console.WriteLine("{0} <= 0, it is wrong",N);
+                return;
+            }
+            Int32 fakt = 1;
+            for (int i = 2; i <= N; i++)
+            {
+                fakt *= i;
+            }
+            Console.WriteLine("There is {0} a possible route of delivery!",fakt);
+            Console.ReadLine();
+        }
+    }
+}
